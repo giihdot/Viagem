b1e386ce5e5a66e68935652c4cd604a66406c358
Kamilly Barra <kamillye033@gmail.com>
2025-03-06 13:47:40 -0300

    Mudando maldivas

--- src/Pages/Maldivas.jsx
+++ src/Pages/Maldivas.jsx
@@ -1,25 +1,52 @@
+import { useState } from "react";
 import "./Maldivas.css";
 
-const destinos = [
-  { nome: "Resort All-Inclusive", descricao: "Tudo incluso.", valor: "R$ 15.700"},
-];
-
 function Maldivas() {
+  const [nota, setNota] = useState(0);
+
+  const selecionarNota = (notaSelecionada) => {
+    setNota(notaSelecionada);
+  };
+
+  const destino = {
+    nome: "Resort All-Inclusive",
+    descricao:
+      "Desfrute de uma estadia luxuosa com tudo incluso! Aproveite praias paradisíacas, gastronomia requintada e serviços exclusivos neste incrível resort nas Maldivas.",
+    valor: "R$ 15.700",
+    imagem: "maldivas1.jpg",
+  };
+
   return (
     <div className="container">
       <h1>Destinos nas Maldivas</h1>
-      <div>
-        {destinos.map((destino, index) => (
-          <div key={index} className="card">
-            <img src={destino.imagem} alt={destino.nome} />
+      <div className="card">
+        <div className="content">
+          <img src={destino.imagem} alt={destino.nome} className="destino-img" />
+          <div className="descricao">
             <h2>{destino.nome}</h2>
             <p>{destino.descricao}</p>
-            <p className="valor">{destino.valor}</p>
           </div>
-        ))}
+        </div>
+
+        <p className="valor">{destino.valor}</p>
+
+        <div className="avaliacao">
+          <h2>Avalie nosso serviço ⭐</h2>
+          {[1, 2, 3, 4, 5].map((numero) => (
+            <span
+              key={numero}
+              onClick={() => selecionarNota(numero)}
+              className={numero <= nota ? "star filled" : "star"}
+            >
+              ★
+            </span>
+          ))}
+          <p>{nota === 0 ? "" : `Você nos avaliou com ${nota} estrela(s)`}</p>
+        </div>
       </div>
     </div>
   );
 }
 
 export default Maldivas;
+
