9faa2e6480535c0663fa387f8525807502e06e2e
giihdot <giihferreira0708@gmail.com>
2025-03-06 13:33:20 -0300

    componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './Pages/Home'
 import Maldivas from "./Pages/Maldivas";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Maldivas />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/Maldivas" element={<Maldivas />} />
       </Routes>
     </Router>
->>>>>>> 07b9757945f2c1c8db8d8b2ba885d6784a4c427a
   );
 }
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,52 +10,6 @@ return (
         <article>
           <Nav />
           <section> 
-          <h3> As Gatitas: A Força Coletiva da Intensidade e da Fé </h3>
-
-<p className="pp">
-  <b> As Gatitas </b>são um grupo unido pela intensidade de suas
-  emoções, pela profundidade de suas crenças e pela força inabalável
-  que nasce da união. Com uma identidade coletiva que transcende as
-  individualidades, elas representam a combinação de forças opostas
-  que se complementam, criando uma energia única e poderosa.
-</p>
-<p className="pp">
-  Movidas pela fé e pela busca por autenticidade, elas percorrem
-  juntas um caminho de desafios, arte e descoberta. Com uma base
-  sólida de crenças espirituais, que vão desde a fé cristã até a
-  conexão com guias espirituais, têm suas forças ancoradas na
-  espiritualidade, que as guia em momentos de incerteza e as
-  impulsiona em direção aos seus objetivos. Sua espiritualidade é a
-  força silenciosa que as sustenta, sempre presente, mesmo nos
-  momentos mais desafiadores. A união das Gatitas é marcada por uma
-  sensibilidade profunda e uma visão crítica do mundo. Elas não se
-  contentam com o óbvio; observam, questionam e buscam significados
-  além das aparências. Cada emoção, cada detalhe, é vivido com uma
-  intensidade que define seu olhar sobre a vida, tornando a
-  experiência coletiva do grupo mais rica e complexa.
-</p>
-
-<p className="pp">
-  Sua energia é multifacetada: a força de liderar e o poder de
-  transformar os desafios em oportunidades coexistem com a
-  delicadeza de um abraço acolhedor e a capacidade de proteger uns
-  aos outros com um amor imenso. Ao mesmo tempo em que se destacam
-  pela coragem de expressar suas opiniões e emoções, há também uma
-  profunda sensibilidade que as torna unidas na busca por um
-  propósito comum.
-  <b> As Gatitas </b> são a união de intensidade, fé e arte. Elas se
-  destacam pela criatividade que permeia todos os aspectos de suas
-  vidas, encontrando beleza e significado nas formas mais simples e
-  nas mais complexas expressões artísticas. A arte, em suas diversas
-  formas, é uma das ferramentas mais poderosas de transformação do
-  grupo, capaz de transformar qualquer momento de incerteza em uma
-  oportunidade de reinvenção. Coletivamente, elas não seguem os
-  padrões ou se conformam com o comum. Em vez disso, desafiam o
-  status quo e buscam sempre ir além, com autenticidade e coragem.
-  As Gatitas são a personificação de uma força coletiva que combina
-  sensibilidade, reflexão, poder, arte e fé em uma jornada conjunta,
-  onde cada passo dado é um reflexo da força da união que as move.
-</p>
 </section>
 </article>
 <Footer />
