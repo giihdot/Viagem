a6b7985b51a6ea2c3f80b7292fadf46e02b403c8
giihdot <giihferreira0708@gmail.com>
2025-03-12 08:04:39 -0300

    footer de novo

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
     <br/>
       {/* Seção com 4 itens lado a lado */}
       <div className="row">
-        <div className="item">
+        <div className="item_2">
         <p> <b>Sobre a Empresa:</b> </p>
 
         <p> Atendimento ao Cliente </p>
@@ -33,7 +33,7 @@ const Footer = () => {
         <p> Excluir minha conta </p>
         </div>
 
-        <div className="item">
+        <div className="item_2">
         <p> <b> Madames Voyage: Agência de Viagens Online </b> </p>
 
         <p> Passagens Aéreas Promocionais </p>
@@ -43,25 +43,19 @@ const Footer = () => {
         <p> Passagem para Recife </p>
         <p> Passagem para Bahia </p>
         <p> Passagem para Santa Catarina </p>
-        <p> Passagem para Maceió </p>
         </div>
 
-        <div className="item">
-        <br/>
-        <br/>
+        <div className="item_3">
         <p> <b> Companhias Aéreas Internacionais </b> </p>
 
         <p> Tap Air Portugal </p>
         <p> Copa Airlines </p>
-        <p> Aerolíneas Argentinas </p>
         <p> Air France </p>
         <p> Iberia </p>
         <p> American Airlines </p>
         </div>
 
-        <div className="item">
-        <br/>
-        <br/>
+        <div className="item_3">
         <p> <b> Companhias Aéreas Nacionais </b> </p>
 
         <p> Azul Viagens </p>
@@ -77,13 +71,17 @@ const Footer = () => {
         <img className="Img_2" src={Logo} alt="Viagem" />
       </div>
 
+    <br/> 
+
       {/* Seção com mais itens abaixo da imagem */}
       <div className="row">
         <div className="item">
+          <br/>
           <p> MadamesVoyage.com Ltda. – CNPJ/ME 40.59.12/0008-76 </p>
  
           <p> Madames Voyage é uma marca do Grupo As Gatitas e sua razão social é MadamesVoyage.com Ltda. 
               Ministério do Turismo - Cadastur 42.395627.30.0009-5 / 42.395627.30.0009-5 – CNPJ/ME 40.59.12/0008-76. 
+              <br/>
               Todos os direitos reservados. Avenida Ana Lombardi, 242, 8º andar, Jardim Canudos, Vinhedo, São Paulo, CEP 13269-540. </p>
 
           <p> Copyright 1992-2024, MademesVoyage.com Ltda </p>
@@ -91,10 +89,12 @@ const Footer = () => {
         </div>
 
         <div className="item">
+          <br/>
           <p>
           A Madames Voyage comercializa os produtos de seus fornecedores de forma direta pelo seu site (www.mademesvoyage.com.br), 
           aplicativo e televendas. Não há qualquer venda por redes sociais (Facebook, Instagram, Twitter, LinkedIn, TikTok, etc.).
           Pós-vendas - os canais oficiais de atendimento aos clientes são: por meio do telefone (19) 97864-2324 e Minhas Viagens. 
+          <br/>
           A Madames Voyage não realiza qualquer tipo de abordagem nas redes sociais ou sites de relacionamento, tampouco disponibiliza 
           e-mails de contato com domínios distintos de @madamesvoyage.com.br. Endereços de e-mails com domínios: @hotmail.com @gmail.com @outlook.com 
           ou qualquer outro neste sentido não tem qualquer relação com a Madames Voyage. Mais esclarecimentos em www.madamesvoyage.com.br
