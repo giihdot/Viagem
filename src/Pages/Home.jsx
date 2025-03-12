654449de4232aef5b61d1c0dbc38e2b2de6a70a0
giihdot <giihferreira0708@gmail.com>
2025-03-12 14:00:36 -0300

    home again 2.0

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -51,7 +51,7 @@ function Home() {
       <div className="container">
       <div className="block">
         <h6> ÍNDIA </h6>
-        <p> A Índia é um país vasto e diverso, conhecido por sua rica história, cultura vibrante e tradições milenares. <br/>
+        <p className="parag"> A Índia é um país vasto e diverso, conhecido por sua rica história, cultura vibrante e tradições milenares. <br/>
         Desde os templos antigos e monumentos como o Taj Mahal até as cores intensas de suas festas, como o Holi, a Índia encanta com sua variedade. <br/>
         Com uma culinária única e uma espiritualidade marcante, é um destino fascinante para quem busca novas experiências. <br/>
         Sua mistura de modernidade e tradições torna o país ainda mais fascinante. </p>
@@ -60,7 +60,7 @@ function Home() {
 
       <div className="block">
         <h6> FINLÂNDIA </h6>
-        <p> A Finlândia é um país nórdico conhecido por sua paisagem deslumbrante, composta por florestas, lagos e o fenômeno da aurora boreal. <br/>
+        <p className="parag"> A Finlândia é um país nórdico conhecido por sua paisagem deslumbrante, composta por florestas, lagos e o fenômeno da aurora boreal. <br/>
         Com uma cultura que valoriza a educação, bem-estar e a sustentabilidade, é um dos lugares mais avançados em qualidade de vida. <br/>
         Helsinque, sua capital, combina modernidade com tradição. Além disso, a Finlândia é famosa por ser o lar do Papai Noel e por suas saunas únicas.</p>
         <img className="Img" src={Finlandia} alt="Finlandia" />
@@ -68,7 +68,7 @@ function Home() {
 
       <div className="block">
         <h6> EGITO </h6>
-        <p> O Egito é um país localizado no nordeste da África, famoso por sua rica história e antigas civilizações. <br/>
+        <p className="parag"> O Egito é um país localizado no nordeste da África, famoso por sua rica história e antigas civilizações. <br/>
         As pirâmides de Gizé e a Esfinge são alguns dos maiores marcos históricos do mundo. O rio Nilo, que corta o país, tem grande importância cultural e econômica. <br/>
         O Egito também é conhecido por sua religião e mitologia, além de ser um destino turístico popular. <br/>
         Sua capital, Cairo, é uma das maiores cidades do mundo árabe. </p>
